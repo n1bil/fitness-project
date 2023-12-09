95dd8cf57b8a44e99401851891edd4833c75029b
n1bil <nabil.nadzor@gmail.com>
2023-12-09 21:27:02 +0100

    Navbar Menu

--- prettier.config.cjs
+++
@@ -1,4 +0,0 @@
-// prettier.config.cjs
-/* eslint-disable */
-exports.plugins = [(await import('prettier-plugin-tailwindcss')).default];
-/* eslint-enable */
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-
+import { useState } from "react";
+import Navbar from "@/scenes/navbar";
 
 function App() {
+    const [selectedPage, setSelectedPage] = useState<string>("home");
 
-  return (
-    <div className="app">app
-      
-    </div>
-  )
+    return (
+        <div className="app bg-gray-20">
+            <Navbar
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
